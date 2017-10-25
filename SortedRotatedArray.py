84bfb09ef08e25ff0c38d0e69af327131ed912d9
DaveRoox <davexrusso@gmail.com>
2017-10-25 15:08:45 +0200

    Update SortedRotatedArray.py

--- SortedRotatedArray.py
+++ SortedRotatedArray.py
@@ -1,18 +1,27 @@
+# Brute-force approach:
+# Time complexity: O(n)
+# Space complexity: O(1)
 def linear_search(array, a, b, target):
 
 	if target < array[a] or target > array[b]:
-        return 'Not found'
+        return False, None, 1
 
     n = len(array)
+	iterations = 1
+	
     for i in range(n):
 		if array[i] == target:
-			return i
+			return True, i, iterations
+		iterations += 1
 
-    return 'Not found'
+    return False, None, iterations
 
-def logarithmic_search(array, a, b, target): # Based on the binary search
+# Binary search based approach:
+# Time complexity: O(lgn)
+# Space complexity: O(1)
+def logarithmic_search(array, a, b, target):
 
-	    if target < array[a] or target > array[b]:
+	if target < array[a] or target > array[b]:
         return False, None, 1
 
     n = len(array)
@@ -30,12 +39,14 @@ def logarithmic_search(array, a, b, target): # Based on the binary search
     return array[c] == target, c if array[c] == target else None, iterations
 
 
+# Main program to test the functions.
+# Creating the array and filling in positions shifted by an offset
 n, offset = 100, 78
-target = 20
 array = [0 for _ in range(n)]
 for i in range(n):
     array[(i + offset) % n] = i * 2
 
+# Printing the array
 for n in array:
     print(n, end='\t')
 print()
@@ -43,6 +54,15 @@ for n in range(len(array)):
     print(n, end='\t')
 print()
 
+# Searching for a target number
+target = 20 # present
+found, index, iterations = rotated_binary_search(array, a=offset, b=offset - 1, target=target)
+if found:
+    print('%d found at index %d [%d iterations]' % (target, index, iterations))
+else:
+    print('%d not found [%d iterations]' % (target, iterations))
+	
+target = 19 # not present
 found, index, iterations = rotated_binary_search(array, a=offset, b=offset - 1, target=target)
 if found:
     print('%d found at index %d [%d iterations]' % (target, index, iterations))
